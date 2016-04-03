7edc2280414f1f4dcf63febd04de7fbff1c0f4c2
ashleybertrand <ashleykaybertrand@gmail.com>
2016-04-03 17:04:45 -0600

    All functions work on valid inputs

--- lab3.scm
+++ lab3.scm
@@ -27,12 +27,17 @@
 ;Set? function:
 (define (set? list)
 (cond
+	;empty list is a set
 	((null? list) #t)
+	
+	;first element in list exists somewhere else in list
 	((not (not (member? (car list) (cdr list)))) #f)
+	
+	;recursively call set? on list without the first element
 	(else (set? (cdr list)))
 ))
 
-;Testing:
+;Testing set?:
 (display "Testing set?:\n")
 (display (set? '(x y z))); −→ #t
 (display " should be true\n")
@@ -46,20 +51,52 @@
 (display " should be false\n\n")
 
 ;Union function:
-;(define (union list1 list2)
+(define (union list1 list2)
+;CHECK IF VALID INPUT (list1 and list2 must be sets)
+;IF NOT VALID, PRINT ERROR MESSAGE, REMOVE DUPLICATES, CALL union ON NEW LISTS
 
-;)
+(cond
+	;if list2 is null, continue using list1
+    ((null? list2) list1)
+    
+    ;checking if the first element in list2 is in list1  
+	((member? (car list2) list1)
+	          
+	;recursively calling union on list1 and list2 without the first element
+    (union list1 (cdr list2)))
+   
+    ;combine the first element of list2 with list1
+    ;recursively call union on this list with list2 without the first element
+    (#t (union (cons (car list2) list1) (cdr list2))) 
+))
 
 ;Intersect function:
-;(define (intersect list1 list2)
-	
-;)
+(define (intersect list1 list2)
+;CHECK IF VALID INPUT (list1 and list2 must be sets)
+;IF NOT VALID, PRINT ERROR MESSAGE, REMOVE DUPLICATES, CALL intersect ON NEW LISTS	
+
+;if list1 is null, continue as if it were an empty list
+(if (null? list1) '()
+    ;checking if the first element in list1 is in list2
+    (let ((included (member? (car list1) list2)))
+    
+    ;if null, continue using list1 without its first element
+    (if (null? (cdr list1))
+        (if included list1 '())
+        (if included
+            ;recursively call intersect on list1 without the first element and list2
+            ;combine this result with the first element from list1
+            (cons (car list1) (intersect (cdr list1) list2))
+            
+            ;recursively call intersect on list1 without the first element and list2
+            (intersect (cdr list1) list2))))	
+))
 
 ;Lab Questions:
 (display "Lab Questions:")
 
 (display "\n1. ")
-;(display (f '((2 7 8 1 3 9)))
+(display (f '(2 7 8 1 3 9)))
          
 (display "\n6. ")
 (display (member? 'one '(1 2 3 4 5)))
@@ -74,7 +111,7 @@
 (display (set? '(it was the best of times, it was the worst of times)))
 
 (display "\n10. ")
-;(display (union '(blue eggs and cheese) '(ham and sandwich)))
+(display (union '(blue eggs and cheese) '(ham and sandwich)))
 
 (display "\n11. ")
-;(display (intersect '(blue cheese and cherry) '(cheese and blue eggs))
+(display (intersect '(blue cheese and cherry) '(cheese and blue eggs)))
